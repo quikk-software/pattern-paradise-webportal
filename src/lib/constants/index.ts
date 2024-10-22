ce6364a7529aa69187cd54df3f2521342b6bdff9
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2024-10-22 10:27:07 +0200

    🔍 Add meta tags

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,1 +1,3 @@
-﻿export const APP_NAME = 'Crochet by Jasmin';
+﻿export const APP_NAME = 'Pattern Paradise';
+export const APP_DESCRIPTION = 'Find patterns from creatives around the world!';
+export const APP_DOMAIN = 'https://cbj-ecru.vercel.app';
