988627b931eef3a4b6a54e23855928908985ba4e
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2024-10-23 08:42:09 +0200

    💄 Improve bottom navigation links

--- src/components/bottom-navigation.tsx
+++ src/components/bottom-navigation.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { Store, Tag, TestTube2 } from 'lucide-react';
 import { cn } from '@/lib/utils';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const menuItems = [
   { id: 'shop', label: 'Shop', icon: Store, href: '/' },
@@ -13,6 +13,8 @@ const menuItems = [
 
 export function BottomNavigation() {
   const [activeItem, setActiveItem] = useState('shop');
+
+  const { push } = useRouter();
 
   return (
     <div
@@ -25,20 +27,21 @@ export function BottomNavigation() {
             key={item.id}
             type="button"
             className={cn(
-              'px-5 hover:bg-muted/50 group transition-colors',
+              'px-5 hover:bg-muted/50 group transition-colors inline-flex flex-col items-center justify-center',
               activeItem === item.id && 'text-primary',
             )}
-            onClick={() => setActiveItem(item.id)}
+            onClick={() => {
+              setActiveItem(item.id);
+              push(item.href);
+            }}
           >
-            <Link href={item.href} className="inline-flex flex-col items-center justify-center">
-              <item.icon
-                className={cn(
-                  'w-6 h-6 mb-1 transition-transform group-hover:scale-110',
-                  activeItem === item.id && 'animate-bounce',
-                )}
-              />
-              <span className="text-xs leading-none">{item.label}</span>
-            </Link>
+            <item.icon
+              className={cn(
+                'w-6 h-6 mb-1 transition-transform group-hover:scale-110',
+                activeItem === item.id && 'animate-bounce',
+              )}
+            />
+            <span className="text-xs leading-none">{item.label}</span>
           </button>
         ))}
       </div>
