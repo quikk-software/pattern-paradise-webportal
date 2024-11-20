e570a98c7c4c4d6713f4692a117de547d4842ac3
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2024-11-20 20:17:50 +0100

    ⏪ Revert to one column for product listings

--- src/components/listing.tsx
+++ src/components/listing.tsx
@@ -187,7 +187,7 @@ export function ListingComponent({ listingType, defaultProducts }: ListingCompon
           </Button>
         </div>
 
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {products.map((product) => (
             <Card key={product.id} className="flex flex-col justify-between">
               <CardContent className="pt-4">
