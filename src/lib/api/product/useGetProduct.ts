6a393d410dfe5e6b1a7f5ce7c0fd56ccc5b25962
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-01-07 12:18:21 +0100

    🐛 Add auth header to get user account and product hooks

--- src/lib/api/product/useGetProduct.ts
+++ src/lib/api/product/useGetProduct.ts
@@ -1,15 +1,25 @@
 import { useState } from 'react';
-import { client } from '@/@types';
+import { client, getApi } from '@/@types';
 import type { GetProductResponse } from '@/@types/api-types';
 import { useApiStates } from '../useApiStates';
+import { useDispatch, useSelector } from 'react-redux';
+import { Store } from '@/lib/redux/store';
 
 export const useGetProduct = () => {
   const [data, setData] = useState<GetProductResponse | undefined>(undefined);
 
   const { handleFn, ...apiStates } = useApiStates();
 
+  const dispatch = useDispatch();
+  const { accessToken, refreshToken } = useSelector((s: Store) => s.auth);
+
   const fetch = async (productId: string) => {
-    const response = await handleFn(async () => await client.api.getProductById(productId));
+    const response = await handleFn(
+      async () =>
+        await client.api.getProductById(productId, {
+          ...(await getApi(accessToken, refreshToken, dispatch)),
+        }),
+    );
 
     setData(response?.data);
 
