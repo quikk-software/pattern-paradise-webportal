8f35be5bff84274e6e1e59e1901ad352ab20cfe9
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-01-09 11:41:09 +0100

    ♻️ Refactor pagination hook

--- src/lib/api/usePagination.ts
+++ src/lib/api/usePagination.ts
@@ -1,5 +1,4 @@
-import { useCallback, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 export const usePagination = (
   initialPageNumber: number = 1,
@@ -13,34 +12,29 @@ export const usePagination = (
   const [pageNumber, setPageNumber] = useState<number>(initialPageNumber);
   const [pageSize, setPageSize] = useState<number>(initialPageSize);
 
-  const dispatch = useDispatch();
-
-  const handlePaginationPayload = useCallback(
-    (payload: any) => {
-      setCount(payload?.count ?? 0);
-      setTotalPages(payload?.totalPages ?? 0);
-      setHasPreviousPage(payload?.hasPreviousPage ?? false);
-      setHasNextPage(payload?.hasNextPage ?? false);
-      if (Boolean(payload?.hasNextPage)) {
-        let newPageNumber = initialPageNumber;
-        if (payload?.pageNumber !== undefined && autoPagination) {
-          newPageNumber = Number(payload.pageNumber) + 1;
-        }
-        setPageNumber(newPageNumber);
+  const handlePaginationPayload = (payload: any) => {
+    setCount(payload?.count ?? 0);
+    setTotalPages(payload?.totalPages ?? 0);
+    setHasPreviousPage(payload?.hasPreviousPage ?? false);
+    setHasNextPage(payload?.hasNextPage ?? false);
+    if (Boolean(payload?.hasNextPage)) {
+      let newPageNumber = initialPageNumber;
+      if (payload?.pageNumber !== undefined && autoPagination) {
+        newPageNumber = Number(payload.pageNumber) + 1;
       }
-      setPageSize(payload?.pageSize ?? initialPageSize);
-    },
-    [initialPageSize, initialPageNumber, dispatch],
-  );
+      setPageNumber(newPageNumber);
+    }
+    setPageSize(payload?.pageSize ?? initialPageSize);
+  };
 
-  const reset = useCallback(() => {
+  const reset = () => {
     setCount(0);
     setTotalPages(0);
     setHasPreviousPage(false);
     setHasNextPage(true);
     setPageNumber(initialPageNumber);
     setPageSize(initialPageSize);
-  }, [initialPageNumber, initialPageSize, dispatch]);
+  };
 
   return {
     handlePaginationPayload,
