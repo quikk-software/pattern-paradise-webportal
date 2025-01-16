291b477c27c606b6f44cd4bc9180ba6702c491d5
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-01-16 13:56:24 +0100

    ♻️ Refactor auth guard

--- src/lib/auth/AuthGuard.tsx
+++ src/lib/auth/AuthGuard.tsx
@@ -1,14 +1,10 @@
 'use client';
 
-import React, { PropsWithChildren, useCallback, useEffect } from 'react';
+import React, { PropsWithChildren, useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Store } from '@/lib/redux/store';
 import { isTokenValid, refreshAccessToken, setUserDataInReduxStore } from '@/lib/auth/auth.utils';
-import {
-  setAccessToken,
-  setCheckAuthIsLoading,
-  setRefreshToken,
-} from '@/lib/features/auth/authSlice';
+import { setAccessToken, setRefreshToken } from '@/lib/features/auth/authSlice';
 import logger from '@/lib/core/logger';
 import { useCookies } from 'next-client-cookies';
 import { usePathname, useRouter } from 'next/navigation';
@@ -25,82 +21,59 @@ const AuthGuard: React.FunctionComponent<PropsWithChildren<Record<never, any>>> 
   const cookieStore = useCookies();
   const allSearchParams = useGetAllSearchParams();
 
-  const {
-    accessToken: accessTokenFromStore,
-    refreshToken: refreshTokenFromStore,
-    checkAuthIsLoading,
-  } = useSelector((store: Store) => store.auth);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const checkAuth = async (
-    accessTokenFromStore: string | null,
-    refreshTokenFromStore: string | null,
-    pathname: string,
-  ) => {
-    dispatch(setCheckAuthIsLoading(true));
+  const { accessToken: accessTokenFromStore, refreshToken: refreshTokenFromStore } = useSelector(
+    (store: Store) => store.auth,
+  );
+
+  const checkAuth = useCallback(async () => {
     try {
+      logger.debug(`Token check not running`);
+
       logger.debug(`Checking Access for <${pathname}>.`);
 
       const accessTokenFromCookies = cookieStore.get('accessToken') ?? null;
       const refreshTokenFromCookies = cookieStore.get('refreshToken') ?? null;
 
-      let accessToken = null;
-      let refreshToken = null;
-
-      if (isTokenValid(accessTokenFromStore)) {
-        accessToken = accessTokenFromStore;
-      } else if (isTokenValid(accessTokenFromCookies)) {
-        refreshToken = accessTokenFromCookies;
-      }
-
-      if (isTokenValid(refreshTokenFromStore)) {
-        accessToken = refreshTokenFromStore;
-      } else if (isTokenValid(refreshTokenFromCookies)) {
-        refreshToken = refreshTokenFromCookies;
-      }
+      let accessToken = accessTokenFromStore || accessTokenFromCookies;
+      let refreshToken = refreshTokenFromStore || refreshTokenFromCookies;
 
       const query = allSearchParams ? buildQueryString(allSearchParams) : null;
       const redirect = query ? `${pathname}?${query}` : pathname;
-      const encodedRedirect = query ? encodeURIComponent(`${pathname}?${query}`) : pathname;
+      const encodedRedirect = encodeURIComponent(redirect);
 
       if (isTokenValid(accessToken)) {
-        logger.debug(`Access token from store is valid.`);
-        logger.debug(`Redirect user to <${pathname}>.`);
-
+        logger.debug('Access token is valid.');
         dispatch(setAccessToken(accessToken));
         dispatch(setRefreshToken(refreshToken));
-
         setUserDataInReduxStore(accessToken!, dispatch);
-
-        dispatch(setCheckAuthIsLoading(false));
-
         router.push(redirect);
+      } else if (isTokenValid(refreshToken)) {
+        logger.debug('Access token is invalid. Attempting to refresh.');
+        await refreshAccessToken(refreshToken, dispatch, cookieStore);
       } else {
-        logger.debug(`Access token from store is invalid. Get access token from refresh token.`);
-
-        if (refreshToken === null || !isTokenValid(refreshToken)) {
-          logger.debug(`Refresh token from store is invalid. User will be logged out...`);
-
-          router.push(`/auth/login?redirect=${encodedRedirect}`);
-        } else {
-          await refreshAccessToken(refreshToken, dispatch, cookieStore);
-        }
+        logger.debug('Both tokens are invalid. Redirecting to login.');
+        router.push(`/auth/login?redirect=${encodedRedirect}`);
       }
     } finally {
-      dispatch(setCheckAuthIsLoading(false));
+      setIsLoading(false);
     }
-  };
+  }, [
+    pathname,
+    allSearchParams,
+    dispatch,
+    router,
+    cookieStore,
+    accessTokenFromStore,
+    refreshTokenFromStore,
+  ]);
 
   useEffect(() => {
-    if (isTokenValid(accessTokenFromStore)) {
-      setUserDataInReduxStore(accessTokenFromStore!, dispatch);
-      return;
-    }
-    checkAuth(accessTokenFromStore, refreshTokenFromStore, pathname).finally(() =>
-      dispatch(setCheckAuthIsLoading(false)),
-    );
-  }, [accessTokenFromStore, refreshTokenFromStore, pathname]);
+    checkAuth();
+  }, []);
 
-  if (checkAuthIsLoading) {
+  if (isLoading) {
     return (
       <div className="min-h-screen flex justify-center items-center">
         <LoadingSpinnerComponent />
