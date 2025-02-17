42a8e6f118337d45d62fa933564e42396b1bc5fb
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-02-17 14:32:54 +0100

    ♻️ Increase currency input font size

--- src/components/price-filter.tsx
+++ src/components/price-filter.tsx
@@ -81,7 +81,7 @@ export default function PriceFilter({
               decimalsLimit={2}
               onValueChange={(value) => handleMinPriceChange(value)}
               className={cn(
-                'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+                'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-md ring-offset-background file:border-0 file:bg-transparent file:text-md file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
               )}
               onKeyDown={handleKeyDown}
             />
@@ -94,7 +94,7 @@ export default function PriceFilter({
               decimalsLimit={2}
               onValueChange={(value) => handleMaxPriceChange(value)}
               className={cn(
-                'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+                'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-md ring-offset-background file:border-0 file:bg-transparent file:text-md file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
               )}
               onKeyDown={handleKeyDown}
             />
--- src/lib/components/PayPalButton/index.tsx
+++ src/lib/components/PayPalButton/index.tsx
@@ -87,7 +87,7 @@ export function PayPalButton({
                   );
                 }}
                 className={cn(
-                  'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-md ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+                  'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-md ring-offset-background file:border-0 file:bg-transparent file:text-md file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
                 )}
                 onKeyDown={handleKeyDown}
               />
