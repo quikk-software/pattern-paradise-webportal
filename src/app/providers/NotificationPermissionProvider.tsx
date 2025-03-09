31603405ab9ea9048716b1465bdb734ffe90b63d
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-03-09 14:39:35 +0100

    ♻️ Dont show notification preferences dialog if user ID is not set yet

--- src/app/providers/NotificationPermissionProvider.tsx
+++ src/app/providers/NotificationPermissionProvider.tsx
@@ -3,9 +3,17 @@
 import React, { useState } from 'react';
 import NotificationPreferences from '@/lib/components/NotificationPreferences';
 import { Dialog, DialogContent } from '@/components/ui/dialog';
+import { useSelector } from 'react-redux';
+import { Store } from '@/lib/redux/store';
 
 export default function NotificationPermissionProvider() {
   const [isDialogOpen, setIsDialogOpen] = useState(true);
+
+  const { userId } = useSelector((s: Store) => s.auth);
+
+  if (!userId) {
+    return null;
+  }
 
   return (
     <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
