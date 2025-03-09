7777d13c7476d6b6eb294f7eef21cf21f5dab8dd
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-03-09 10:06:08 +0100

    ♻️ Explicitely check window is not undefined

--- src/lib/core/serviceWorkerRegister.tsx
+++ src/lib/core/serviceWorkerRegister.tsx
@@ -5,32 +5,30 @@ import logger from '@/lib/core/logger';
 
 export default function ServiceWorkerRegister() {
   useEffect(() => {
-    if (typeof window === 'undefined') {
-      return;
-    }
+    if (typeof window !== 'undefined') {
+      if ('serviceWorker' in window.navigator) {
+        const registerServiceWorker = async () => {
+          try {
+            const registration = await window.navigator.serviceWorker.register('/sw.js');
+            logger.log('Service Worker registered with scope:', registration.scope);
+          } catch (error) {
+            logger.error('Service Worker registration failed:', error);
+          }
+        };
 
-    if ('serviceWorker' in window.navigator) {
-      const registerServiceWorker = async () => {
-        try {
-          const registration = await window.navigator.serviceWorker.register('/sw.js');
-          logger.log('Service Worker registered with scope:', registration.scope);
-        } catch (error) {
-          logger.error('Service Worker registration failed:', error);
-        }
-      };
+        const registerFirebaseServiceWorker = async () => {
+          try {
+            const registration = await window.navigator.serviceWorker.register(
+              '/firebase-messaging-sw.js',
+            );
+            logger.log('Firebase Messaging Service Worker registered:', registration.scope);
+          } catch (error) {
+            logger.error('Firebase Service Worker registration failed:', error);
+          }
+        };
 
-      const registerFirebaseServiceWorker = async () => {
-        try {
-          const registration = await window.navigator.serviceWorker.register(
-            '/firebase-messaging-sw.js',
-          );
-          logger.log('Firebase Messaging Service Worker registered:', registration.scope);
-        } catch (error) {
-          logger.error('Firebase Service Worker registration failed:', error);
-        }
-      };
-
-      registerServiceWorker().then(() => registerFirebaseServiceWorker());
+        registerServiceWorker().then(() => registerFirebaseServiceWorker());
+      }
     }
   }, []);
 
