ed335edf6677599394dbf2e8fe9efc5908b31176
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-03-09 13:00:20 +0100

    🔧 Update firebase storage bucket URL

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,7 +5,7 @@ firebase.initializeApp({
   apiKey: 'AIzaSyDZK-s64gEzXF8dsTP42w3T2rlD3OKgWpI',
   authDomain: 'pattern-paradise.firebaseapp.com',
   projectId: 'pattern-paradise',
-  storageBucket: 'pattern-paradise.firebasestorage.app',
+  storageBucket: 'pattern-paradise.appspot.com',
   messagingSenderId: '608358089878',
   appId: '1:608358089878:web:3acc6039e99f44d86aeeb7',
   measurementId: 'G-DYG4Q4N3JJ',
--- src/lib/notifications/device-token.ts
+++ src/lib/notifications/device-token.ts
@@ -8,7 +8,7 @@ export const firebaseConfig = {
   apiKey: 'AIzaSyDZK-s64gEzXF8dsTP42w3T2rlD3OKgWpI',
   authDomain: 'pattern-paradise.firebaseapp.com',
   projectId: 'pattern-paradise',
-  storageBucket: 'pattern-paradise.firebasestorage.app',
+  storageBucket: 'pattern-paradise.appspot.com',
   messagingSenderId: '608358089878',
   appId: '1:608358089878:web:3acc6039e99f44d86aeeb7',
   measurementId: 'G-DYG4Q4N3JJ',
