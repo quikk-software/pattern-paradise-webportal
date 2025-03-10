55b4ee5ce4dd0b30e4fe3e154fdf652b255418a2
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-03-10 20:01:29 +0100

    ♻️ Refactor service workers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,9 @@ import AuthSessionProvider from '@/app/providers/AuthSessionProvider';
 import DynamicPaddingWrapper from '@/app/wrappers/DynamicPaddingWrapper';
 import ComingSoon from '@/components/coming-soon';
 import CookieConsentBanner from '@/lib/components/CookieConsentBanner';
-import { ServiceWorkerProvider } from '@/app/providers/ServiceWorkerProvider';
+import { registerServiceWorker } from '@/lib/workers/serviceWorkerRegistration';
+
+registerServiceWorker();
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -98,18 +100,16 @@ export default async function RootLayout({
         <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-hidden`}>
-        <ServiceWorkerProvider>
-          <CookiesProvider>
-            {maintenanceMode ? (
-              <ComingSoon />
-            ) : (
-              <AuthSessionProvider>
-                <DynamicPaddingWrapper>{children}</DynamicPaddingWrapper>
-              </AuthSessionProvider>
-            )}
-            <CookieConsentBanner maintenanceMode={maintenanceMode} />
-          </CookiesProvider>
-        </ServiceWorkerProvider>
+        <CookiesProvider>
+          {maintenanceMode ? (
+            <ComingSoon />
+          ) : (
+            <AuthSessionProvider>
+              <DynamicPaddingWrapper>{children}</DynamicPaddingWrapper>
+            </AuthSessionProvider>
+          )}
+          <CookieConsentBanner maintenanceMode={maintenanceMode} />
+        </CookiesProvider>
       </body>
     </html>
   );
