09400df84535af59f6ab2bac6770baaa944ccec0
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-03-10 18:53:25 +0100

    ♻️ Register firebase service worker

--- src/lib/core/serviceWorkerRegister.tsx
+++ src/lib/core/serviceWorkerRegister.tsx
@@ -7,16 +7,27 @@ export default function ServiceWorkerRegister() {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       if ('serviceWorker' in window.navigator) {
-        const registerServiceWorker = async () => {
+        const registerMainServiceWorker = async () => {
           try {
             const registration = await window.navigator.serviceWorker.register('/sw.js');
-            logger.log('Service Worker registered with scope:', registration.scope);
+            logger.log('Main Service Worker registered with scope:', registration.scope);
           } catch (error) {
-            logger.error('Service Worker registration failed:', error);
+            logger.error('Main Service Worker registration failed:', error);
           }
         };
 
-        registerServiceWorker().then();
+        const registerFirebaseServiceWorker = async () => {
+          try {
+            const registration = await window.navigator.serviceWorker.register(
+              '/firebase-messaging-sw.js',
+            );
+            logger.log('Firebase Service Worker registered with scope:', registration.scope);
+          } catch (error) {
+            logger.error('Firebase Service Worker registration failed:', error);
+          }
+        };
+
+        registerMainServiceWorker().then(() => registerFirebaseServiceWorker());
       }
     }
   }, []);
