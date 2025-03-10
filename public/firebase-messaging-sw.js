dbd49fa689d6ab96358292dddd4c5a49bbf457e6
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-03-10 21:17:14 +0100

    ♻️ Update service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -10,7 +10,7 @@ firebase.initializeApp({
   apiKey: 'AIzaSyDZK-s64gEzXF8dsTP42w3T2rlD3OKgWpI',
   authDomain: 'pattern-paradise.firebaseapp.com',
   projectId: 'pattern-paradise',
-  storageBucket: 'pattern-paradise.appspot.com',
+  storageBucket: 'pattern-paradise.firebasestorage.app',
   messagingSenderId: '608358089878',
   appId: '1:608358089878:web:3acc6039e99f44d86aeeb7',
   measurementId: 'G-DYG4Q4N3JJ',
