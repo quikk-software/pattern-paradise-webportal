d4b7b2022c22a7b9d9246f5f6e78a750450cffa8
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-03-13 13:04:18 +0100

    ♻️ Refactor registration

--- src/lib/components/CookieConsentBanner/index.tsx
+++ src/lib/components/CookieConsentBanner/index.tsx
@@ -41,55 +41,55 @@ export default function CookieConsentBanner({ maintenanceMode }: CookieConsentBa
     setVisible(false);
   };
 
-  if (isApp(window)) {
-    return null;
-  }
-
-  return (
-    <>
-      {consentGiven ? <Analytics /> : null}
-      {visible ? (
-        <CookieConsent
-          location="bottom"
-          buttonText="Accept"
-          declineButtonText="Decline"
-          cookieName={COOKIE_CONSENT_NAME}
-          style={{ background: 'none', position: 'relative' }}
-          contentStyle={{ margin: 0 }}
-          buttonStyle={{ display: 'none' }}
-          declineButtonStyle={{ display: 'none' }}
-          expires={150}
-          onAccept={handleAccept}
-          onDecline={handleDecline}
-        >
-          <div className="fixed bottom-0 left-0 w-full z-50 bg-background border-t border-border shadow-lg p-4">
-            <div className="items-center justify-between gap-4">
-              <div className="flex flex-col gap-4">
-                <div className="flex flex-col gap-2">
-                  <h2 className="text-lg font-semibold mb-2 text-primary">
-                    We value your privacy 🍪
-                  </h2>
-                  <p className="text-sm text-muted-foreground">
-                    We use cookies to enhance your browsing experience. By clicking
-                    &apos;Accept&apos;, you consent to our use of cookies.
-                  </p>
-                </div>
-                <div className="flex gap-2 justify-end">
-                  {maintenanceMode ? null : (
-                    <Link rel={'nofollow'} href="/terms-and-privacy?action=scrollToPrivacyPolicy">
-                      <Button variant="link">Privacy Policy</Button>
-                    </Link>
-                  )}
-                  <Button variant="secondary" onClick={handleDecline}>
-                    Decline
-                  </Button>
-                  <Button onClick={handleAccept}>Accept</Button>
+  if (typeof window !== 'undefined' && !isApp(window)) {
+    return (
+      <>
+        {consentGiven ? <Analytics /> : null}
+        {visible ? (
+          <CookieConsent
+            location="bottom"
+            buttonText="Accept"
+            declineButtonText="Decline"
+            cookieName={COOKIE_CONSENT_NAME}
+            style={{ background: 'none', position: 'relative' }}
+            contentStyle={{ margin: 0 }}
+            buttonStyle={{ display: 'none' }}
+            declineButtonStyle={{ display: 'none' }}
+            expires={150}
+            onAccept={handleAccept}
+            onDecline={handleDecline}
+          >
+            <div className="fixed bottom-0 left-0 w-full z-50 bg-background border-t border-border shadow-lg p-4">
+              <div className="items-center justify-between gap-4">
+                <div className="flex flex-col gap-4">
+                  <div className="flex flex-col gap-2">
+                    <h2 className="text-lg font-semibold mb-2 text-primary">
+                      We value your privacy 🍪
+                    </h2>
+                    <p className="text-sm text-muted-foreground">
+                      We use cookies to enhance your browsing experience. By clicking
+                      &apos;Accept&apos;, you consent to our use of cookies.
+                    </p>
+                  </div>
+                  <div className="flex gap-2 justify-end">
+                    {maintenanceMode ? null : (
+                      <Link rel={'nofollow'} href="/terms-and-privacy?action=scrollToPrivacyPolicy">
+                        <Button variant="link">Privacy Policy</Button>
+                      </Link>
+                    )}
+                    <Button variant="secondary" onClick={handleDecline}>
+                      Decline
+                    </Button>
+                    <Button onClick={handleAccept}>Accept</Button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </CookieConsent>
-      ) : null}
-    </>
-  );
+          </CookieConsent>
+        ) : null}
+      </>
+    );
+  }
+
+  return null;
 }
