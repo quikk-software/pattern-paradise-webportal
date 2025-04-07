428b3bcb3424e4655fbd392b1d2b97fb4dd08f3f
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-04-07 09:11:57 +0200

    ♻️ Fix capture status display

--- src/components/order-details.tsx
+++ src/components/order-details.tsx
@@ -52,7 +52,7 @@ export function OrderDetails({ order }: OrderDetailsProps) {
     return <LoadingSpinnerComponent />;
   }
 
-  const isPayed = order.status === 'APPROVED' || order.status === 'COMPLETED';
+  const isPayed = order.status === 'CAPTURED' || order.status === 'COMPLETED';
   const isPending = order.status === 'PENDING';
   const isCreated = order.status === 'CREATED';
   const isSeller = order.seller.id === userId;
