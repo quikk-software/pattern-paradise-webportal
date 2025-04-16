b8226305dee6fb4d7941f6ae1d0926a439f2b575
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-04-16 07:59:39 +0200

    🐛 Track with ref if update has been tried

--- src/hooks/useValidSession.tsx
+++ src/hooks/useValidSession.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useSession, signOut } from 'next-auth/react';
 import logger from '@/lib/core/logger';
 import { usePathname } from 'next/navigation';
@@ -7,6 +7,7 @@ import { buildQueryString } from '@/lib/utils';
 
 export const useValidSession = () => {
   const { data, status, update } = useSession();
+  const hasTriedUpdate = useRef(false);
 
   const pathname = usePathname();
   const allSearchParams = useGetAllSearchParams();
@@ -15,18 +16,23 @@ export const useValidSession = () => {
   const redirect = query ? `${pathname}?${query}` : pathname;
   const encodedRedirect = encodeURIComponent(redirect);
 
-  // useEffect(() => {
-  //   if (status === 'authenticated') {
-  //     const expiresAt = data?.user?.expiresAt;
-  //     if (expiresAt && Date.now() > expiresAt) {
-  //       logger.warn('Access token expired. Update...');
-  //       update().catch(() => {
-  //         logger.warn('Update failed. Sign out...');
-  //         signOut({ callbackUrl: `/auth/login?redirect=${encodedRedirect}` }).then();
-  //       });
-  //     }
-  //   }
-  // }, [data, status]);
+  useEffect(() => {
+    if (status === 'authenticated') {
+      const expiresAt = data?.user?.expiresAt;
+
+      if (expiresAt && Date.now() > expiresAt) {
+        if (!hasTriedUpdate.current) {
+          hasTriedUpdate.current = true;
+          logger.warn('Access token expired. Attempting update...');
+
+          update().catch(() => {
+            logger.warn('Update failed. Signing out...');
+            signOut({ callbackUrl: `/auth/login?redirect=${encodedRedirect}` });
+          });
+        }
+      }
+    }
+  }, [data, status]);
 
   return { data, status, update };
 };
