35074cf0f2a600ae55a9c326936614b08f244808
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-04-30 15:46:36 +0200

    ✨ Inform user that Google login is not available in app

--- src/lib/components/GoogleLoginButton/index.tsx
+++ src/lib/components/GoogleLoginButton/index.tsx
@@ -6,6 +6,8 @@ import Image from 'next/image';
 import GoogleLogo from '@/assets/logos/google-logo.svg';
 import { Button } from '@/components/ui/button';
 import { isIOSMode } from '@/lib/core/utils';
+import GoogleLoginUnavailableDrawer from '@/lib/components/GoogleLoginUnavailableDrawer';
+import RequestPasswordDrawer from '@/lib/components/RequestPasswordDrawer';
 
 interface GoogleLoginButtonProps {
   callbackUrl?: string;
@@ -13,26 +15,45 @@ interface GoogleLoginButtonProps {
 
 export default function GoogleLoginButton({ callbackUrl }: GoogleLoginButtonProps) {
   const [isStandalone, setIsStandalone] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isRequestPasswordDrawerOpen, setIsRequestPasswordDrawerOpen] = useState(false);
 
   useEffect(() => {
     setIsStandalone(isIOSMode());
   }, []);
 
-  if (!isStandalone) {
-    return (
+  const handleLogin = async () => {
+    if (isStandalone) {
+      setIsOpen(true);
+      return;
+    }
+    await signIn('google', {
+      callbackUrl,
+    });
+  };
+
+  return (
+    <>
       <Button
-        onClick={() =>
-          signIn('keycloak-google', {
-            callbackUrl,
-          })
-        }
+        onClick={() => handleLogin()}
         className="w-full flex items-center justify-center gap-2 bg-white shadow hover:shadow-md hover:bg-gray-50 transition duration-150"
       >
         <Image src={GoogleLogo} alt="Google Logo" className="w-5 h-5" width={50} height={50} />
         <span className="text-sm font-medium text-gray-700">Login with Google</span>
       </Button>
-    );
-  }
-
-  return null;
+      <GoogleLoginUnavailableDrawer
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        onLogin={() => setIsOpen(false)}
+        onResetPassword={() => {
+          setIsOpen(false);
+          setIsRequestPasswordDrawerOpen(true);
+        }}
+      />
+      <RequestPasswordDrawer
+        drawerIsOpen={isRequestPasswordDrawerOpen}
+        setDrawerIsOpen={setIsRequestPasswordDrawerOpen}
+      />
+    </>
+  );
 }
