58c748c627a3f2fa878bbec2f68fe99b16613f58
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-05-07 14:38:05 +0200

    🔍 Add og logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -91,6 +91,8 @@ export default async function RootLayout({
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Pattern Paradise" />
 
+        <meta name="og:logo" content={`${APP_DOMAIN}/favicons/apple-icon-precomposed.png`} />
+
         <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
         <link rel="manifest" href="/manifest.webmanifest" />
