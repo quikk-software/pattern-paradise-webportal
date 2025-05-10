588613cb7bf62dd51fe8bd03b9ab42576cb8b4b9
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-05-10 13:25:10 +0200

    🐛 Use sale price before normal price

--- src/lib/components/BuyNowButton/index.tsx
+++ src/lib/components/BuyNowButton/index.tsx
@@ -189,7 +189,7 @@ export function BuyNowButton({ product, customPriceDisabled = false }: BuyNowBut
               </div>
               <CheckoutButtons
                 disabled={!isLoggedIn}
-                price={customPrice ?? product.price}
+                price={customPrice ?? product.salePrice ?? product.price}
                 product={product}
                 country={country}
               />
