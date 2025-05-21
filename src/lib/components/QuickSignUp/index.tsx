c0860a57b500ae707d34e0e45f2fd0b8440ff03a
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-05-21 16:26:08 +0200

    ♻️ Update quick sign up

--- src/lib/components/QuickSignUp/index.tsx
+++ src/lib/components/QuickSignUp/index.tsx
@@ -5,13 +5,13 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { EMAIL_REGEX, PASSWORD_REGEX, PASSWORD_REGEX_MESSAGE } from '@/lib/constants';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
-import { Rabbit, ShieldCheck } from 'lucide-react';
-import { useCreateUser } from '@/lib/api';
+import { Check, Rabbit, ShieldCheck, X } from 'lucide-react';
+import { useCheckEmail, useCreateUser } from '@/lib/api';
 import RequestStatus from '@/lib/components/RequestStatus';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Label } from '@/components/ui/label';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { PasswordValidationChecklist } from '@/lib/components/PasswordValidationChecklist';
 import useAuth from '@/lib/auth/useAuth';
 import GoogleLoginButton from '@/lib/components/GoogleLoginButton';
@@ -28,8 +28,15 @@ interface QuickSignUpProps {
 
 export default function QuickSignUp({ redirect }: QuickSignUpProps) {
   const [isLoading, setIsLoading] = useState(false);
+  const [debouncedEmail, setDebouncedEmail] = useState('');
 
   const { mutate, isSuccess, isError, errorDetail } = useCreateUser();
+  const {
+    mutate: checkEmail,
+    isSuccess: checkEmailIsSuccess,
+    isError: checkEmailIsError,
+    errorDetail: checkEmailErrorDetail,
+  } = useCheckEmail();
 
   const {
     handleLogin,
@@ -43,6 +50,26 @@ export default function QuickSignUp({ redirect }: QuickSignUpProps) {
     watch,
     formState: { errors },
   } = useForm<FormValues>();
+
+  const email = watch('email');
+  const password = watch('password');
+
+  useEffect(() => {
+    if (!debouncedEmail) {
+      return;
+    }
+    checkEmail(debouncedEmail).then();
+  }, [debouncedEmail]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      if (email) {
+        setDebouncedEmail(email.toLowerCase().trim());
+      }
+    }, 500);
+
+    return () => clearTimeout(timerId);
+  }, [email]);
 
   const onSubmit: SubmitHandler<FormValues> = async (data) => {
     setIsLoading(true);
@@ -75,8 +102,6 @@ export default function QuickSignUp({ redirect }: QuickSignUpProps) {
       setIsLoading(false);
     }
   };
-
-  const password = watch('password');
 
   if (isSuccess) {
     return (
@@ -129,72 +154,94 @@ export default function QuickSignUp({ redirect }: QuickSignUpProps) {
               {errors.email && (
                 <p className="text-sm text-left text-red-500">{errors.email.message}</p>
               )}
+              {debouncedEmail && checkEmailIsSuccess && !errors.email ? (
+                <p className="text-green-500 text-sm flex gap-1 items-center">
+                  <Check className="h-4 w-4" /> Email is available
+                </p>
+              ) : null}
+              {debouncedEmail && checkEmailIsError ? (
+                <p className="text-red-500 text-sm flex gap-1 items-center">
+                  <X className="h-4 w-4" /> {checkEmailErrorDetail ?? 'Email is not available'}
+                </p>
+              ) : null}
             </div>
 
-            <div className="space-y-1">
-              <Input
-                id="password"
-                type="password"
-                placeholder="Enter a password"
-                {...register('password', {
-                  required: 'Password is required',
-                  pattern: {
-                    value: PASSWORD_REGEX,
-                    message: PASSWORD_REGEX_MESSAGE,
-                  },
-                  onChange: (e) => {
-                    e.target.value = e.target.value.trim();
-                  },
-                })}
-              />
-              {password ? <PasswordValidationChecklist password={password} /> : null}
-            </div>
-          </div>
-
-          <div className="space-y-2">
-            <div className="flex gap-4 items-center">
-              <Controller
-                name="hasAcceptedTermsAndPrivacy"
-                control={control}
-                rules={{
-                  required:
-                    'You must accept the Privacy Policy and Terms and Conditions to proceed',
-                }}
-                render={({ field }) => (
-                  <Checkbox
-                    id="hasAcceptedTermsAndPrivacy"
-                    checked={field.value}
-                    onCheckedChange={field.onChange}
-                  />
-                )}
-              />
-              <Label htmlFor="hasAcceptedTermsAndPrivacy" className="block text-sm text-left">
-                I confirm that I have read and agree to the{' '}
-                <Link
-                  rel={'nofollow'}
-                  href="/terms-and-privacy?action=scrollToPrivacyPolicy"
-                  target="_blank"
-                  className="text-blue-500 underline"
-                >
-                  Privacy Policy
-                </Link>{' '}
-                and{' '}
-                <Link
-                  href="/terms-and-privacy?action=scrollToTermsAndConditions"
-                  target="_blank"
-                  className="text-blue-500 underline"
-                >
-                  Terms and Conditions
-                </Link>
-                .
-              </Label>
-            </div>
-            {errors.hasAcceptedTermsAndPrivacy ? (
-              <p className="text-sm text-red-500 mb-2 text-left">
-                {errors.hasAcceptedTermsAndPrivacy.message as string}
-              </p>
+            {checkEmailIsSuccess && !errors.email ? (
+              <div className="space-y-1 transition duration-300 ease-in">
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="Enter a password"
+                  {...register('password', {
+                    required: 'Password is required',
+                    pattern: {
+                      value: PASSWORD_REGEX,
+                      message: PASSWORD_REGEX_MESSAGE,
+                    },
+                    onChange: (e) => {
+                      e.target.value = e.target.value.trim();
+                    },
+                  })}
+                />
+                {!password && errors.password ? (
+                  <p className="text-sm text-red-500 mb-2 text-left">
+                    {errors.password.message as string}
+                  </p>
+                ) : null}
+                {password ? <PasswordValidationChecklist password={password} /> : null}
+              </div>
             ) : null}
           </div>
+
+          {checkEmailIsSuccess && !errors.email ? (
+            <div className="space-y-2 transition duration-300 ease-in">
+              <div className="flex gap-4 items-center">
+                <Controller
+                  name="hasAcceptedTermsAndPrivacy"
+                  control={control}
+                  rules={{
+                    required:
+                      'You must accept the Privacy Policy and Terms and Conditions to proceed',
+                  }}
+                  render={({ field }) => (
+                    <Checkbox
+                      id="hasAcceptedTermsAndPrivacy"
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  )}
+                />
+                <Label
+                  htmlFor="hasAcceptedTermsAndPrivacy"
+                  className="block text-sm text-left cursor-pointer"
+                >
+                  I confirm that I have read and agree to the{' '}
+                  <Link
+                    rel={'nofollow'}
+                    href="/terms-and-privacy?action=scrollToPrivacyPolicy"
+                    target="_blank"
+                    className="text-blue-500 underline"
+                  >
+                    Privacy Policy
+                  </Link>{' '}
+                  and{' '}
+                  <Link
+                    href="/terms-and-privacy?action=scrollToTermsAndConditions"
+                    target="_blank"
+                    className="text-blue-500 underline"
+                  >
+                    Terms and Conditions
+                  </Link>
+                  .
+                </Label>
+              </div>
+              {errors.hasAcceptedTermsAndPrivacy ? (
+                <p className="text-sm text-red-500 mb-2 text-left">
+                  {errors.hasAcceptedTermsAndPrivacy.message as string}
+                </p>
+              ) : null}
+            </div>
+          ) : null}
 
           <RequestStatus
             isSuccess={isSuccess}
@@ -224,7 +271,17 @@ export default function QuickSignUp({ redirect }: QuickSignUpProps) {
               </span>
             }
           />
-          <Button type="submit" className="w-full" disabled={isLoading}>
+          <Button
+            type="submit"
+            className="w-full"
+            disabled={
+              isLoading ||
+              checkEmailIsError ||
+              !!errors.email ||
+              !!errors.password ||
+              !!errors.hasAcceptedTermsAndPrivacy
+            }
+          >
             {isLoading ? 'Signing up...' : 'Sign up'}
           </Button>
         </form>
