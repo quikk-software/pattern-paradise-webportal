99f9123d62975cea37307f67fed8d4d29ff8e61c
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-05-23 13:19:22 +0200

    ♻️ Add welcome banner to browse page

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { ListingComponent } from '@/components/listing';
 import { generatePageMetadata } from '@/lib/core/metadata';
+import WelcomeBannerWrapper from '@/lib/components/WelcomeBannerWrapper';
 
 export const metadata = generatePageMetadata('/browse');
 
 export default function BrowsePage({ searchParams }: { searchParams: { [key: string]: string } }) {
-  return <ListingComponent status={'Released'} initialQuery={searchParams} />;
+  return (
+    <div className="space-y-4">
+      <WelcomeBannerWrapper redirect="/browse" />
+      <ListingComponent status={'Released'} initialQuery={searchParams} />
+    </div>
+  );
 }
