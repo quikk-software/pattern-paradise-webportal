fc9d7ca8d7a4341650462fe3a4f74a0548282b73
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-06-16 08:50:10 +0200

    ✨ Add load more button to tester application selection

--- src/app/[lang]/app/secure/sell/testings/[testingId]/page.tsx
+++ src/app/[lang]/app/secure/sell/testings/[testingId]/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import { useGetTesting, useListTesterApplications } from '@/lib/api/testing';
 import { TesterApplicantsPage } from '@/components/tester-applicants-page';
+import { Button } from '@/components/ui/button';
 
 export default function TestingPage({ params }: { params: { testingId: string } }) {
   const testingId = params.testingId;
@@ -17,6 +18,7 @@ export default function TestingPage({ params }: { params: { testingId: string } 
     reset,
     totalCount,
     isLoading: fetchTesterApplicationsIsLoading,
+    hasNextPage: fetchTesterApplicationsHasNextPage,
   } = useListTesterApplications({});
   const { fetch: fetchTesting, data: testing } = useGetTesting();
 
@@ -34,31 +36,48 @@ export default function TestingPage({ params }: { params: { testingId: string } 
   }, [testingId]);
 
   return (
-    <TesterApplicantsPage
-      applications={data}
-      directionFn={(overrideDirection: 'asc' | 'desc') => {
-        if (overrideDirection !== direction) {
+    <div className="flex flex-col gap-4">
+      <TesterApplicantsPage
+        applications={data}
+        directionFn={(overrideDirection: 'asc' | 'desc') => {
+          if (overrideDirection !== direction) {
+            setData([]);
+            reset();
+          }
+
+          setDirection(overrideDirection);
+        }}
+        sortKeyFn={(overrideSortKey: 'updatedAt' | 'assignedAt') => {
+          if (overrideSortKey !== sortKey) {
+            reset();
+          }
+          setSortKey(overrideSortKey);
+        }}
+        filterFn={(overrideFilter: string[]) => {
           setData([]);
           reset();
-        }
-
-        setDirection(overrideDirection);
-      }}
-      sortKeyFn={(overrideSortKey: 'updatedAt' | 'assignedAt') => {
-        if (overrideSortKey !== sortKey) {
-          reset();
-        }
-        setSortKey(overrideSortKey);
-      }}
-      filterFn={(overrideFilter: string[]) => {
-        setData([]);
-        reset();
-        setFilter(overrideFilter);
-      }}
-      filter={filter}
-      totalApplicantsCount={totalCount}
-      testing={testing}
-      fetchTesterApplicationsIsLoading={fetchTesterApplicationsIsLoading}
-    />
+          setFilter(overrideFilter);
+        }}
+        filter={filter}
+        totalApplicantsCount={totalCount}
+        testing={testing}
+        fetchTesterApplicationsIsLoading={fetchTesterApplicationsIsLoading}
+      />
+      {!fetchTesterApplicationsIsLoading && fetchTesterApplicationsHasNextPage ? (
+        <Button
+          className="w-full"
+          onClick={() =>
+            fetch(testingId, {
+              direction,
+              sortKey,
+              filter,
+              status: ['Created'],
+            })
+          }
+        >
+          Load More
+        </Button>
+      ) : null}
+    </div>
   );
 }
