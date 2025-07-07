7e6add247b43581bc26010d1726f9bc468476e87
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2025-07-07 16:19:05 +0200

    ♻️ Upgrade dependencies and fix width and height for product images

--- src/components/animated-landing.tsx
+++ src/components/animated-landing.tsx
@@ -26,7 +26,7 @@ export default function AnimatedLanding() {
       opacity: 1,
       y: 0,
       transition: {
-        type: 'spring',
+        type: 'spring' as const,
         stiffness: 100,
         damping: 10,
       },
